74af34bc121e6da8917c30cde1dcea60521477ee
Azin <setiguy@gachon.ac.kr>
2024-07-17 11:53:58 +0900

    test

--- hwan/mountain/app/src/main/java/com/example/mountain/MainActivity.kt
+++ hwan/mountain/app/src/main/java/com/example/mountain/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    //ㅌㅔ스트용
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
