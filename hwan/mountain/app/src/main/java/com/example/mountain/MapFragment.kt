b26d4776a16cc081f03937de7fd3b5a24788fdf0
Lee seung hwan <hwan00716@gmail.com>
2024-07-19 23:47:31 +0900

    네이버 지도 띄우기

--- hwan/mountain/app/src/main/java/com/example/mountain/MapFragment.kt
+++ hwan/mountain/app/src/main/java/com/example/mountain/MapFragment.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.naver.maps.map.MapView
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.geometry.LatLng
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +21,48 @@ class MapFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        // 네이버 지도가 준비되면 기본 위치를 서울로 설정
+        val seoul = LatLng(37.5665, 126.9780)
+        naverMap.moveCamera(CameraUpdate.scrollTo(seoul))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
