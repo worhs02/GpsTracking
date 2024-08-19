aa18c4dd11f5f8162f2551a7514e11802a20cfc7
Lee seung hwan <hwan00716@gmail.com>
2024-08-19 21:10:56 +0900

    인원 프래그먼트에 UI(GIF 파일) 추가

--- hwan/mountain/app/src/main/java/com/example/mountain/PeopleFragment.kt
+++ hwan/mountain/app/src/main/java/com/example/mountain/PeopleFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 
 class PeopleFragment : Fragment() {
 
@@ -20,8 +22,15 @@ class PeopleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mountainAnimationView: ImageView = view.findViewById(R.id.mountain_animation_view)
         val createRoomButton: Button = view.findViewById(R.id.create_room_button)
         val joinRoomButton: Button = view.findViewById(R.id.join_room_button)
+
+        // GIF를 ImageView에 로드
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.mountain_animation) // drawable 폴더에 있는 GIF 파일 이름
+            .into(mountainAnimationView)
 
         createRoomButton.setOnClickListener {
             // RoomCreateFragment로 이동
